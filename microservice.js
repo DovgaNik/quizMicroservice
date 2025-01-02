a14d2c01f6e82fa37e45b1b6b9001b4ddf1ce3ec
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-01-02 17:33:18 +0100

    Deleted the old version of the endpoint

--- microservice.js
+++ microservice.js
@@ -12,28 +12,14 @@ const API_key = config.GEMINI_API_KEY;
 
 app.use(express.json())
 
-app.post("/gemini",  async (req, res) => {
+app.post("/gemini", async (req, res) => {
 
     const genAI = new GoogleGenerativeAI(API_key);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
-
-    const prompt = req.body.prompt + req.body.content;
-
-    const result = await model.generateContent(prompt);
-    console.log(result.response.text());
-    res.send(result.response.text());
-
-})
-
-app.post("/gemini/options", async (req, res) => {
-
-    const genAI = new GoogleGenerativeAI(API_key);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash-exp" });
 
     const prompt = prompts.createPrompt(req.body.amountOfQuestions, req.body.amountOfOptions, req.body.amountOfMultipleAnswerQuestions, req.body.ignoreContext, req.body.content);
 
     const result = await model.generateContent(prompt);
-    console.log(result.response.text());
     res.send(result.response.text());
 
 })
